1a68f4527ae78fc11f653e0ae1f0bd376cc382dc
XSilentDevilX <121007102+XSilentDevilX@users.noreply.github.com>
2025-06-26 18:36:16 +1000

    Update Silent Currency
    Added shapeless crafting recipes for all coins as per Vigilii's request.
    
    Added shapeless recipe for emeralds --> emerald coins and vice versa as per Vigiliis' request

--- kubejs/server_scripts/Silent Currency.js
+++ kubejs/server_scripts/Silent Currency.js
@@ -115,6 +115,118 @@ event.custom({
   "results": [{"count": 8, "id": "kubejs:gold_coins"}]        
 })
 
+//Shapeless Crafting Recipes///
+
+//Emerald Coins --> Emeralds
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [{"item": "kubejs:emerald_coins"}],
+  "result": {"count": 1, "id": "minecraft:emerald"}
+})
+
+//Emeralds --> Emerald Coins
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [{"item": "minecraft:emerald"}],
+  "result": {"count": 1, "id": "kubejs:emerald_coins"}
+})
+
+///Coins Downward///
+
+//Netherite Coin --> Energite Coins
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [{"item": "kubejs:netherite_coins"}],
+  "result": {"count": 8, "id": "kubejs:energite_coins"}
+})
+
+//Energite Coin --> Diamond Coins
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [{"item": "kubejs:energite_coins"}],
+  "result": {"count": 8, "id": "kubejs:diamond_coins"}
+})
+
+//Diamond Coin --> Emerald Coins
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [{"item": "kubejs:diamond_coins"}],
+  "result": {"count": 8, "id": "kubejs:emerald_coins"}
+})
+
+//Emerald Coin --> Gold Coins
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [{"item": "kubejs:emerald_coins"}],
+  "result": {"count": 8, "id": "kubejs:gold_coins"}
+})
+
+///Coins Upward///
+
+//Gold Coins --> Emerald Coin
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [
+    {"item": "kubejs:gold_coins"},
+    {"item": "kubejs:gold_coins"},
+    {"item": "kubejs:gold_coins"},
+    {"item": "kubejs:gold_coins"},
+    {"item": "kubejs:gold_coins"},
+    {"item": "kubejs:gold_coins"},
+    {"item": "kubejs:gold_coins"},
+    {"item": "kubejs:gold_coins"}
+  ],
+  "result": {"count": 1, "id": "kubejs:emerald_coins"}
+})
+
+//Emerald Coins --> Diamond Coin
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [
+    {"item": "kubejs:emerald_coins"},
+    {"item": "kubejs:emerald_coins"},
+    {"item": "kubejs:emerald_coins"},
+    {"item": "kubejs:emerald_coins"},
+    {"item": "kubejs:emerald_coins"},
+    {"item": "kubejs:emerald_coins"},
+    {"item": "kubejs:emerald_coins"},
+    {"item": "kubejs:emerald_coins"}
+  ],
+  "result": {"count": 1, "id": "kubejs:diamond_coins"}
+})
+
+//Diamond Coins --> Energite Coin
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [
+    {"item": "kubejs:diamond_coins"},
+    {"item": "kubejs:diamond_coins"},
+    {"item": "kubejs:diamond_coins"},
+    {"item": "kubejs:diamond_coins"},
+    {"item": "kubejs:diamond_coins"},
+    {"item": "kubejs:diamond_coins"},
+    {"item": "kubejs:diamond_coins"},
+    {"item": "kubejs:diamond_coins"}
+  ],
+  "result": {"count": 1, "id": "kubejs:energite_coins"}
+})
+
+//Energite Coins --> Netherite Coin
+event.custom({
+  "type": "minecraft:crafting_shapeless",
+  "ingredients": [
+    {"item": "kubejs:energite_coins"},
+    {"item": "kubejs:energite_coins"},
+    {"item": "kubejs:energite_coins"},
+    {"item": "kubejs:energite_coins"},
+    {"item": "kubejs:energite_coins"},
+    {"item": "kubejs:energite_coins"},
+    {"item": "kubejs:energite_coins"},
+    {"item": "kubejs:energite_coins"}
+  ],
+  "result": {"count": 1, "id": "kubejs:netherite_coins"}
+})
+
 //event.stonecutting('kubejs:shop', 'minecraft:paper')
 //event.stonecutting('kubejs:sell', 'minecraft:paper')
 //event.stonecutting('kubejs:buy', 'minecraft:paper')
